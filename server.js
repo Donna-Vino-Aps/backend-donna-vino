b4c086f06e632cb3fe1d1ff06211b06dcf33c9ac
lenin del rio <lenindelrionicaragua@gmail.com>
2025-01-21 09:54:26 +0100

    feat: add configuration files for Heroku, Jest, and Node version management

---
+++ server.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app.js";
+import { logInfo, logError } from "./util/logging.js";
+import connectDB from "./db/connectDB.js";
+
+// The environment should set the port
+const port = process.env.PORT;
+if (port == null) {
+  // If this fails, make sure you have created a `.env` file in the right place with the PORT set
+  logError(new Error("Cannot find a PORT number, did you create a .env file?"));
+}
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      logInfo(`Server started on port ${port}`);
+    });
+  } catch (error) {
+    logError(error);
+  }
+};
+
+// Start the server
+startServer();
---
+++ src/app.js
@@ -0,0 +1,49 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+// import { requireAuth } from "./middleware/authMiddleware.js";
+// import userRouter from "./routes/userRoutes.js";
+// import authRouter from "./routes/authRoutes.js";
+
+dotenv.config();
+
+// Create an express server
+const app = express();
+
+// Tell express to use the json middleware
+app.use(express.json());
+
+app.use(cookieParser());
+
+const allowedOrigins = [
+  "http://localhost:8081",
+  "http://192.168.178.182:3000",
+  "https://habit-tracker-app-front.netlify.app",
+  "http://192.168.178.182:8081",
+];
+
+// CORS configuration
+app.use(
+  cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  }),
+);
+
+/****** Attach routes ******/
+/**
+ * We use /api/ at the start of every route!
+ * As we also host our client code on heroku we want to separate the API endpoints.
+ */
+
+// app.use("/api/auth", authRouter);
+// app.use("/api/user", requireAuth, userRouter);
+
+export default app;
