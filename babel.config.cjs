bb3da39e08888e1d48b6168d175257bb549debd2
lenin del rio <lenindelrionicaragua@gmail.com>
2025-01-22 10:39:49 +0100

    test: update server.test.js

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,12 +1,5 @@
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          node: "current",
-        },
-      },
-    ],
-  ],
+  presets: [["@babel/preset-env", { targets: "node 16", modules: "auto" }]],
+  plugins: ["@babel/plugin-transform-runtime"],
+  sourceType: "module",
 };
--- config/auth/auth.js
+++ config/auth/auth.js
@@ -1,9 +1,9 @@
-// export const jwtConfig = {
-//   secret: process.env.JWT_SECRET,
-//   expiresIn: "1h",
-// };
+export const jwtConfig = {
+  secret: process.env.JWT_SECRET,
+  expiresIn: "1h",
+};
 
-// export const oauthConfig = {
-//   googleClientId: process.env.GOOGLE_CLIENT_ID,
-//   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-// };
+export const oauthConfig = {
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+};
