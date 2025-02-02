6ff102fe247f2e0153fecf41faa1d93b93591a00
YuejiaoShi <shiyuejiaosyj@gmail.com>
2025-02-03 00:42:55 +0100

    refactor: refactor verifyEmail with  async/await

--- src/controllers/authControllers/emailVerificationController.js
+++ src/controllers/authControllers/emailVerificationController.js
@@ -7,14 +7,14 @@ import UserVerification from "../../models/userVerification.js";
 import User from "../../models/userModels.js";
 import { logError, logInfo } from "../../util/logging.js";
 
-const resolvePath = (relativePath) => {
+export const resolvePath = (relativePath) => {
   const basePath = process.env.GITHUB_ACTIONS
     ? path.join(process.cwd(), "src")
     : __dirname;
   return path.resolve(basePath, relativePath);
 };
 
-// Setting server URL based o the environment
+// Setting server URL based on the environment
 const development = "http://localhost:3000";
 const production = "https://backend-donna-vino-c5c2e1c03c18.herokuapp.com";
 const currentUrl =
@@ -82,7 +82,6 @@ export const resendVerificationLink = async (req, res) => {
     }
 
     await UserVerification.deleteMany({ userId });
-
     const emailSent = await sendVerificationEmail({ _id: userId, email });
 
     if (!emailSent) {
@@ -109,95 +108,57 @@ export const resendVerificationLink = async (req, res) => {
 };
 
 // Function to verify the email
-export const verifyEmail = (req, res) => {
+export const verifyEmail = async (req, res) => {
   logInfo("verifyEmail function called");
   const { userId, uniqueString } = req.params;
   logInfo(`Params - userId: ${userId}, uniqueString: ${uniqueString}`);
 
-  UserVerification.findOne({ userId }) // Find the verification record
-    .then((result) => {
-      if (result) {
-        logInfo("UserVerification record found:", result);
-        const { expiresAt, uniqueString: hashedUniqueString } = result;
+  try {
+    const result = await UserVerification.findOne({ userId });
 
-        if (expiresAt < Date.now()) {
-          // If the link has expired
-          logInfo("Verification link has expired");
-          UserVerification.deleteOne({ userId }) // Delete the expired verification record
-            .then(() => {
-              User.deleteOne({ _id: userId }) // Delete the user
-                .then(() => {
-                  const message = "Link has expired. Please sign up again";
-                  logInfo(message); // Log the message
-                  res.redirect(`/user/verified?error=true&message=${message}`);
-                })
-                .catch((error) => {
-                  logError(error);
-                  const message =
-                    "Clearing user with expired unique string failed.";
-                  logInfo(message, error);
-                  res.redirect(`/user/verified?error=true&message=${message}`);
-                });
-            })
-            .catch((error) => {
-              logError(error);
-              const message =
-                "Clearing expired user verification record failed.";
-              logInfo(message, error);
-              res.redirect(`/user/verified?error=true&message=${message}`);
-            });
-        } else {
-          // If the link is still valid
-          logInfo("Verification link is still valid");
-          bcrypt
-            .compare(uniqueString, hashedUniqueString) // Compare the unique strings
-            .then((match) => {
-              if (match) {
-                // If they match
-                logInfo("Unique strings match");
-                User.updateOne({ _id: userId }, { verified: true }) // Update the user's verified status
-                  .then(() => {
-                    logInfo("User verification status updated successfully");
-                    res.sendFile(resolvePath("views/verified.html"));
-                  })
-                  .catch((error) => {
-                    logError(error);
-                    const message =
-                      "An error occurred while finalizing successful verification.";
-                    logInfo(message, error);
-                    res.redirect(
-                      `/user/verified?error=true&message=${message}`,
-                    );
-                  });
-              } else {
-                // If the unique strings do not match
-                const message =
-                  "Invalid verification details passed. Check your inbox.";
-                logInfo(message);
-                res.redirect(`/user/verified?error=true&message=${message}`);
-              }
-            })
-            .catch((error) => {
-              logError(error);
-              const message =
-                "An error occurred while updating user record to show verified.";
-              logInfo(message, error);
-              res.redirect(`/user/verified?error=true&message=${message}`);
-            });
-        }
-      } else {
-        // If no verification record is found
-        const message =
-          "Account record doesn't exist or has been verified already. Please sign up or log in.";
+    if (result) {
+      logInfo("UserVerification record found:", result);
+      const { expiresAt, uniqueString: hashedUniqueString } = result;
+
+      if (expiresAt < Date.now()) {
+        // If the link has expired
+        logInfo("Verification link has expired");
+        await UserVerification.deleteOne({ userId });
+        await User.deleteOne({ _id: userId });
+        const message = "Link has expired. Please sign up again";
         logInfo(message);
-        res.redirect(`/user/verified?error=true&message=${message}`);
+        return res.redirect(`/user/verified?error=true&message=${message}`);
       }
-    })
-    .catch((error) => {
-      logError(error);
+
+      // If the link is still valid
+      logInfo("Verification link is still valid");
+      const match = await bcrypt.compare(uniqueString, hashedUniqueString);
+
+      if (match) {
+        // If they match
+        logInfo("Unique strings match");
+        await User.updateOne({ _id: userId }, { verified: true });
+        logInfo("User verification status updated successfully");
+        return res.sendFile(resolvePath("views/verified.html"));
+      } else {
+        // If the unique strings do not match
+        const message =
+          "Invalid verification details passed. Check your inbox.";
+        logInfo(message);
+        return res.redirect(`/user/verified?error=true&message=${message}`);
+      }
+    } else {
+      // If no verification record is found
       const message =
-        "An error occurred while checking for existing user verification record";
-      logInfo(message, error);
-      res.redirect(`/user/verified?error=true&message=${message}`);
-    });
+        "Account record doesn't exist or has been verified already. Please sign up or log in.";
+      logInfo(message);
+      return res.redirect(`/user/verified?error=true&message=${message}`);
+    }
+  } catch (error) {
+    logError(error);
+    const message =
+      "An error occurred while checking for existing user verification record";
+    logInfo(message, error);
+    return res.redirect(`/user/verified?error=true&message=${message}`);
+  }
 };
