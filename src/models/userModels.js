7ac9593e90e43b23d33e39d8d4895728fc01b74d
lenin del rio <lenindelrionicaragua@gmail.com>
2025-02-04 20:54:08 +0100

    Fix: Update dateOfBirth validation to use YYYY-MM-DD format

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -42,7 +42,7 @@ export const validateUser = (
 
   if ((requireEmail && userObject.email === null) || userObject.email === "") {
     errorList.push("Email is a required field");
-    // logInfo("User create Validation failed: Email is required field.");
+    logInfo("User create Validation failed: Email is required field.");
   }
 
   if (
@@ -81,13 +81,11 @@ export const validateUser = (
     errorList.push("Date Of Birth is a required field.");
   }
 
-  const isValidDateOfBirth = /^[A-Z][a-z]{2} [A-Z][a-z]{2} \d{2} \d{4}$/.test(
-    userObject.dateOfBirth,
-  );
+  const isValidDateOfBirth = /^\d{4}-\d{2}-\d{2}$/.test(userObject.dateOfBirth);
 
   if (requireDateOfBirth && (!userObject.dateOfBirth || !isValidDateOfBirth)) {
     errorList.push(
-      "Date Of Birth is a required field with valid format (e.g., 'Tue Feb 01 2022').",
+      "Date Of Birth is a required field with valid format (YYYY-MM-DD, e.g., '2024-02-04').",
     );
   }
 
