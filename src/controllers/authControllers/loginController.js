70648dc17517c727eb10ee5b3607b0412f0cc331
YuejiaoShi <shiyuejiaosyj@gmail.com>
2025-02-11 15:33:52 +0100

    refactor:Refactor login, simplify error handling, add JWT expiration, and improve response structure

--- src/controllers/authControllers/loginController.js
+++ src/controllers/authControllers/loginController.js
@@ -1,20 +1,17 @@
-// import { logInfo } from "../../util/logging.js";
 import validationErrorMessage from "../../util/validationErrorMessage.js";
 import User from "../../models/userModels.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 export const login = async (req, res) => {
-  // Check if the request body contains user information
   if (!req.body || typeof req.body !== "object" || !req.body.user) {
     return res
       .status(400)
-      .json({ success: false, msg: "Invalid request body" }); // Update error message here
+      .json({ success: false, msg: "Invalid request body" });
   }
 
   const { email, password, ...additionalFields } = req.body.user;
 
-  // Validation Errors
   try {
     const errors = [];
     if (!email || !password) {
@@ -27,61 +24,53 @@ export const login = async (req, res) => {
 
     if (errors.length > 0) {
       const errorMessage = validationErrorMessage(errors);
-      res.status(400).json({ success: false, error: errorMessage });
-      return;
+      return res.status(400).json({ success: false, error: errorMessage });
     }
 
-    // Continue with the login process if there are no validation errors
-    const userFound = await User.findOne({ email: email });
+    const userFound = await User.findOne({ email });
 
-    if (userFound) {
-      // logInfo(`User found: ${JSON.stringify(userFound)}`);
-
-      const isPasswordValid = await bcrypt.compare(
-        password,
-        userFound.password,
-      );
-
-      // After the comparison
-
-      if (isPasswordValid) {
-        // Create jwt token
-        const token = jwt.sign(
-          { userId: userFound._id.toString() },
-          process.env.JWT_SECRET,
-        );
-
-        // Save token in cookie
-        res.cookie("session", token, {
-          maxAge: 86400000,
-          secure: process.env.NODE_ENV === "production",
-          httpOnly: true,
-          sameSite: "lax",
-        });
-
-        res.status(200).json({
-          success: true,
-          msg: "Login successful",
-          token,
-          user: {
-            id: userFound._id,
-            email: userFound.email,
-            name: userFound.name,
-          },
-        });
-      } else {
-        res.status(401).json({
-          success: false,
-          msg: "The password provided is incorrect. Please verify your password and try again.",
-        });
-      }
-    } else {
-      res.status(401).json({
+    if (!userFound) {
+      return res.status(401).json({
         success: false,
         msg: "No user was found associated with the provided email address. Please verify your email and try again or register if you are a new user.",
       });
     }
+
+    const isPasswordValid = bcrypt.compare(password, userFound.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        success: false,
+        msg: "The password provided is incorrect. Please verify your password and try again.",
+      });
+    }
+
+    const token = jwt.sign(
+      { userId: userFound._id.toString() },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+
+    // Save token in cookie
+    res.cookie("session", token, {
+      maxAge: 86400000, // 1 day
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      sameSite: "lax",
+    });
+
+    res.status(200).json({
+      success: true,
+      msg: "Login successful",
+      token,
+      user: {
+        id: userFound._id,
+        email: userFound.email,
+        name: userFound.name,
+      },
+    });
   } catch (error) {
+    console.error("Login error:", error);
     res.status(500).json({
       success: false,
       msg: "An internal server error occurred. Please try again later or contact technical support if the issue persists.",
