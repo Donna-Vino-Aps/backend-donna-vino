3cbf64aa1d45a6105ce3cc497d9a3a791750cbd6
YuejiaoShi <shiyuejiaosyj@gmail.com>
2025-02-17 21:56:03 +0100

    chore: remove unused import

--- src/controllers/sendEmailControllers/sendEmailController.js
+++ src/controllers/sendEmailControllers/sendEmailController.js
@@ -1,6 +1,5 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 import { sendEmail } from "../../util/emailUtils.js";
 
 const resolvePath = (relativePath) => {
