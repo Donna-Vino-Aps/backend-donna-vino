a834b138cd9cbfb3975c6362b0d52c381d70c330
YuejiaoShi <shiyuejiaosyj@gmail.com>
2025-02-17 21:29:15 +0100

    chore: remove  the old subscribeRoutes and controller

--- src/routes/subscribeRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { subscribeUser } from "../controllers/sendEmailControllers/subscribeController.js";
-
-const subscribeRouter = express.Router();
-
-subscribeRouter.post("/", subscribeUser);
-
-export default subscribeRouter;
