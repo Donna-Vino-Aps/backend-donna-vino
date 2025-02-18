2fef8ef57c1db5e850f57d98d5a427712ff3fe43
YuejiaoShi <shiyuejiaosyj@gmail.com>
2025-02-18 11:22:10 +0100

    fix: modify the title of the email in test file as well

--- src/__tests__/utilsTest/emailUtils.test.js
+++ src/__tests__/utilsTest/emailUtils.test.js
@@ -25,7 +25,7 @@ describe("sendEmail", () => {
     const result = await sendEmail(to, subject, html);
 
     expect(mockSend).toHaveBeenCalledWith({
-      from: process.env.AUTH_EMAIL,
+      from: `"Donna Vino" <${process.env.AUTH_EMAIL}>`,
       to,
       subject,
       html,
@@ -45,7 +45,7 @@ describe("sendEmail", () => {
     await expect(sendEmail(to, subject, html)).rejects.toThrow(mockError);
 
     expect(mockSend).toHaveBeenCalledWith({
-      from: process.env.AUTH_EMAIL,
+      from: `"Donna Vino" <${process.env.AUTH_EMAIL}>`,
       to,
       subject,
       html,
