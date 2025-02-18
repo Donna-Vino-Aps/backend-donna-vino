bc32bbd9aa8f734aa7d5a19f780647a58c0b6bf6
YuejiaoShi <shiyuejiaosyj@gmail.com>
2025-02-18 11:19:17 +0100

    feat: Customize the email sender name to "Donna Vino" instead of "info"

--- src/util/emailUtils.js
+++ src/util/emailUtils.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async (to, subject, html) => {
   try {
     const data = await resend.emails.send({
-      from: process.env.AUTH_EMAIL,
+      from: `"Donna Vino" <${process.env.AUTH_EMAIL}>`,
       to,
       subject,
       html,
