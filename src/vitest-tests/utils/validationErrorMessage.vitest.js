a10eb2feaef25d68cf56b80336cd1977ceb8a48a
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2025-02-20 18:48:31 +0100

    feat: added db test, logging

---
+++ src/vitest-tests/utils/validationErrorMessage.vitest.js
@@ -0,0 +1,18 @@
+import { describe, it, expect, vi } from "vitest";
+
+import validationErrorMessage from "../../util/validationErrorMessage.js";
+
+describe("validationErrorMessage", () => {
+  it("Adds every error message in the given list to the message", () => {
+    const result = validationErrorMessage(["foo", "bar"]);
+    expect(result).toContain("foo");
+    expect(result).toContain("bar");
+  });
+
+  it("Throws an error if the parameter is unexpected", () => {
+    expect(() => validationErrorMessage(null)).toThrow();
+    expect(() =>
+      validationErrorMessage({ errorList: ["foo", "bar"] }),
+    ).toThrow();
+  });
+});
