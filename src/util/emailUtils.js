78088c36b8d7ef23a86e78b5af2b81c416f1f237
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2025-02-24 08:16:07 +0100

    fix(emailsUtils): fixing vitest tests

--- src/util/emailUtils.js
+++ src/util/emailUtils.js
@@ -1,11 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env.test" });
-import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+import { Resend } from "resend";
 
 export const sendEmail = async (to, subject, html) => {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY); // API key is loaded at runtime
+
     const data = await resend.emails.send({
       from: `"Donna Vino" <${process.env.AUTH_EMAIL}>`,
       to,
