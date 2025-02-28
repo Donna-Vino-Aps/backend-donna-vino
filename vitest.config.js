c96bc5a7aca31552697f61cb350bfd10e2ee5d0c
SimonFromSweden <jenssimonberglund@gmail.com>
2025-02-28 12:16:52 +0100

    fix: installed some dependencies

--- vitest.config.js
+++ vitest.config.js
@@ -1,22 +1,25 @@
 import { defineConfig } from "vitest/config";
 import path from "path";
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+import tsconfigPaths from "vite-tsconfig-paths";
+
+  export default defineConfig({
+    plugins: [tsconfigPaths()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  test: {
-    globals: true,
-    environment: "node",
-    include: ["src/vitest-tests/**/*.vitest.{js,ts}"],
-    exclude: [
-      "**/node_modules/**",
-      "**/dist/**",
-      "**/__tests__/**",
-      "**/jest.config.*",
-    ],
-    setupFiles: "./vitest.setup.js",
-  },
-});
+    test: {
+      globals: true,
+      environment: "node",
+      include: ["src/vitest-tests/**/*.vitest.{js,ts}"],
+      exclude: [
+        "**/node_modules/**",
+        "**/dist/**",
+        "**/__tests__/**",
+        "**/jest.config.*",
+      ],
+      setupFiles: "./vitest.setup.js",
+    },
+  });
