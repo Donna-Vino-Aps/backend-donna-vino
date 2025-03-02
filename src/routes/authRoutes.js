a3d6abce1f4e07fd3e9090b957b6dc89ac2c96e8
Alejandro Gispert <158798324+AlejandroGispert@users.noreply.github.com>
2025-03-02 22:09:06 +0100

    feat(resetPasswordController): yet to add a FRONTEND_URL in the .env

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,11 +2,14 @@ import express from "express";
 import { signup } from "../controllers/authControllers/signupController.js";
 import { signInWithGoogleController } from "../controllers/authControllers/signInWithGoogleController.js";
 import { login } from "../controllers/authControllers/loginController.js";
+import { updateUserData } from "../controllers/authControllers/resetPasswordController.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/sign-up", signup);
 authRouter.post("/sign-in-with-google", signInWithGoogleController);
 authRouter.post("/log-in", login);
+
+authRouter.post("/update-user-data", updateUserData);
 
 export default authRouter;
