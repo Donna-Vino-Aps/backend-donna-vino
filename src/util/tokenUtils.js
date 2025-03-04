d0d62b635842c566a5cc006728f10c035cf631c3
lenin del rio <lenindelrionicaragua@gmail.com>
2025-03-04 15:04:26 +0100

    feat: create tokenUtils.js to generate one token with experation date

---
+++ src/util/tokenUtils.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "your_secret_key"; // Cambia por una clave secreta
+
+export const generateToken = (email) => {
+  const expiresIn = "6h";
+
+  const payload = {
+    email: email,
+  };
+
+  return jwt.sign(payload, JWT_SECRET, { expiresIn });
+};
