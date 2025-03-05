b6c808d5f44f4d88c3a697dbbf136459b9943ac1
lenin del rio <lenindelrionicaragua@gmail.com>
2025-03-05 19:03:36 +0100

    feat: add web url variable in the enviroments.js

--- src/config/environment.js
+++ src/config/environment.js
@@ -4,8 +4,15 @@ dotenv.config();
 
 const API_URL_HEROKU = process.env.API_URL_HEROKU;
 const API_URL_LOCAL = process.env.API_URL_LOCAL;
+const DONNA_VINO_WEB_LOCAL = process.env.DONNA_VINO_WEB_LOCAL;
+const DONNA_VINO_WEB_HEROKU = process.env.DONNA_VINO_WEB_HEROKU;
 
 export const baseApiUrl =
   process.env.NODE_ENV === "production" ? API_URL_HEROKU : API_URL_LOCAL;
+
+export const baseDonnaVinoWebUrl =
+  process.env.NODE_ENV === "production"
+    ? DONNA_VINO_WEB_HEROKU
+    : DONNA_VINO_WEB_LOCAL;
 
 logInfo(`Server URL: ${baseApiUrl}`);
--- src/controllers/subscribeControllers/subscribeController.js
+++ src/controllers/subscribeControllers/subscribeController.js
@@ -1,1 +1,39 @@
-export const subscribeController = async (req, res) => {};
+import jwt from "jsonwebtoken";
+import PreSubscribedUser from "../../models/subscribe/preSubscribe.js";
+import { logError } from "../../util/logging.js";
+
+export const subscribeController = async (req, res) => {
+  const token = req.query.token; // Obtener el token desde la URL
+
+  // Verificar si el token está presente
+  if (!token) {
+    return res.redirect("/error"); // Redirigir a una página de error si el token está ausente
+  }
+
+  try {
+    // Verificar y decodificar el token usando la clave secreta
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Usa la clave secreta que configuraste
+    const email = decoded.email; // Suponiendo que el token contiene el email del usuario
+
+    // Buscar al usuario en la colección PreSubscribedUser
+    const user = await PreSubscribedUser.findOne({ email });
+
+    // Si el usuario no se encuentra
+    if (!user) {
+      return res.redirect("/error"); // Redirigir a una página de error si no se encuentra el usuario
+    }
+
+    // Cambiar el estado de la suscripción del usuario a 'true'
+    user.isSubscribed = true;
+
+    // Guardar los cambios en la base de datos
+    await user.save();
+
+    // Redirigir a una página de éxito
+    return res.redirect("/success"); // Redirigir a una página de éxito tras la confirmación
+  } catch (error) {
+    // En caso de error (por ejemplo, token inválido o expirado)
+    logError("Error during subscription confirmation:", error);
+    return res.redirect("/error"); // Redirigir a una página de error en caso de problemas
+  }
+};
