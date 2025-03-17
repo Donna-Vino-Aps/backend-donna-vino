14760fe4d54a635379fd417978037275cd0a29f9
lenin del rio <lenindelrionicaragua@gmail.com>
2025-03-17 12:35:56 +0100

    delete: resenEmail verification.js

--- src/routes/subscribeRoutes.js
+++ src/routes/subscribeRoutes.js
@@ -2,13 +2,11 @@ import express from "express";
 import { preSubscribeController } from "../controllers/subscribeControllers/preSubscribeController.js";
 import { subscribeController } from "../controllers/subscribeControllers/subscribeController.js";
 import { unSubscribeController } from "../controllers/subscribeControllers/unSubscribeController.js";
-import { resendSubscriptionEmailController } from "../controllers/subscribeControllers/resendSubscriptionEmailController.js";
 
 const subscribeRouter = express.Router();
 
 subscribeRouter.post("/pre-subscribe", preSubscribeController);
 subscribeRouter.post("/confirm-subscription", subscribeController);
 subscribeRouter.post("/unsubscribe-request", unSubscribeController);
-subscribeRouter.post("/resend-email", resendSubscriptionEmailController);
 
 export default subscribeRouter;
