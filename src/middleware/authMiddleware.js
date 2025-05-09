2b4d68da233bb5fa312b7331b5fd9161bbaad047
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-05-09 16:31:56 +0200

    fix: streamlining authMiddleware to only check authentication (Increasing separation of concern)

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { logInfo, logError } from "../util/logging.js";
+import { logError } from "../util/logging.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,49 +10,28 @@ if (!process.env.JWT_SECRET) {
 export const requireAuth = (req, res, next) => {
   const session = req.cookies.session?.trim();
   const authHeader = req.headers["authorization"]?.trim();
-  let token;
 
   // If neither a session cookie nor an authorization header is provided, return 401
   if (!session && !authHeader) {
-    logError("No session cookie (or authorization header) found.");
+    logError("Authentication failed.");
     return res.status(401).send({
       success: false,
       msg: "BAD REQUEST: Authentication required.",
     });
   }
 
-  // Use the session cookie if available; otherwise, extract the token from the Authorization header
-  if (session) {
-    token = session;
-    logInfo("Verifying token from session cookie.");
-  } else if (authHeader) {
-    token = authHeader.startsWith("Bearer ")
-      ? authHeader.split(" ")[1]
-      : authHeader;
-    logInfo("Verifying token from Authorization header.");
-  }
+  const token =
+    session ||
+    (authHeader?.startsWith("Bearer ") ? authHeader.split(" ")[1] : authHeader);
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, data) => {
-    if (err) {
-      logError("Error verifying token:", err);
-      return res.status(401).json({
-        success: false,
-        msg: "Unauthorized: Invalid or expired token.",
-      });
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err || !decoded?.userId) {
+      return res
+        .status(401)
+        .json({ success: false, msg: "Invalid or expired token." });
     }
 
-    logInfo("Decoded token data:", data);
-
-    if (!data.userId) {
-      logError("JWT decoded but missing expected userId property:", data);
-      return res.status(401).json({
-        success: false,
-        msg: "Unauthorized: Invalid or expired token.",
-      });
-    }
-
-    // req.userId = data.userId;
-    req.user = { id: data.userId };
+    req.userId = decoded.userId;
     next();
   });
 };
