270cda45a2dae57b50a96ec57f26c532993b5aea
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-06-10 16:54:32 +0200

    reverting unsubscribecontroller

--- src/controllers/subscribeControllers/unSubscribeController.js
+++ src/controllers/subscribeControllers/unSubscribeController.js
@@ -9,17 +9,14 @@ import SubscriptionVerificationToken from "../../models/subscriptionVerification
 dotenv.config();
 
 const resolvePath = (relativePath) => path.resolve(process.cwd(), relativePath);
-
 const unsubscribeSuccessTemplatePath = resolvePath(
   "src/templates/unsubscribeSuccessTemplate.html",
 );
 const unsubscribeConfirmationTemplatePath = resolvePath(
   "src/templates/confirmUnsubscribeTemplate.html",
 );
-
 let emailSuccessTemplate;
 let unsubscribeConfirmationTemplate;
-
 try {
   emailSuccessTemplate = fs.readFileSync(
     unsubscribeSuccessTemplatePath,
@@ -32,7 +29,6 @@ try {
 } catch (error) {
   logError("Error reading email templates", error);
 }
-
 if (!emailSuccessTemplate || !unsubscribeConfirmationTemplate) {
   throw new Error("Email templates not loaded properly");
 }
@@ -40,21 +36,18 @@ if (!emailSuccessTemplate || !unsubscribeConfirmationTemplate) {
 export const unSubscribeController = async (req, res) => {
   try {
     const { token, subject } = req.body;
-
     if (!token || !subject) {
       return res.status(400).json({
         success: false,
         message: "Token and subject are required.",
       });
     }
-
     // Validate token format
     const tokenDoc = await SubscriptionVerificationToken.findOne({
       token,
       expiresAt: { $gt: new Date() },
       used: false,
     });
-
     if (!tokenDoc) {
       logInfo("Invalid or expired token. Unable to unsubscribe.");
       return res.status(401).json({
@@ -63,10 +56,8 @@ export const unSubscribeController = async (req, res) => {
           "Token expired or invalid. Please request a new unsubscribe link.",
       });
     }
-
     // If token is valid and not revoked
     const { email: to } = tokenDoc;
-
     // Verify if the user exists in the subscribed list
     const existingSubscribedUser = await SubscribedUser.findOne({ email: to });
     if (!existingSubscribedUser) {
@@ -76,15 +67,12 @@ export const unSubscribeController = async (req, res) => {
         message: "User does not exist in the subscribed list.",
       });
     }
-
     // Remove the user from the subscription list
     await SubscribedUser.deleteOne({ email: to });
     logInfo(`User ${to} unsubscribed successfully.`);
-
-    // Mark the token as used and save it
+    // Mark the token as used and delete it
     tokenDoc.used = true;
     await tokenDoc.save();
-
     // Send the success email (no token included)
     await sendEmail(
       to,
@@ -92,7 +80,6 @@ export const unSubscribeController = async (req, res) => {
       emailSuccessTemplate,
     );
     logInfo(`Unsubscribe success email sent to ${to}`);
-
     return res.status(200).json({
       success: true,
       message:
