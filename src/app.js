0e86261093d7a0939804a395131967df535869f1
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-08-13 11:24:43 +0200

    Revert "fix: importing cloudinary in router in proper order"
    This reverts commit 6df736fe2afef105dfabdac671418aca90428950.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,38 @@
 import express from "express";
 import morgan from "morgan";
+
+/**
+ * Even though `index.js` is traditionally associated with CommonJS-style `require` usage,
+ * I still recommend using it to encapsulate the internal structure of the routes directory
+ * and simplify imports — treating it like a self-contained module or package.
+ *
+ * While this pattern may not be idiomatic for some JavaScript developers,
+ * it promotes a clean, modular architecture that's engineer-friendly and easy to scale.
+ */
 import router from "./routes/router.js";
+// import { router } from "./routes/index.js";
+
 import cookieParser from "cookie-parser";
 import corsConfig from "./config/cors.js";
 import { authMiddleware } from "./middleware/authMiddleware.js";
 import { globalLimiter } from "./middleware/rateLimitMiddleware.js";
+import cloudinaryRouter from "./routes/cloudinaryRouter.js";
 
+// Create an express server
 const app = express();
 
 app.use(corsConfig);
-app.use(morgan("dev"));
+
+app.use("/api/upload", cloudinaryRouter);
+
+// Debug-only middleware used to log HTTP requests in development.
+// Helps visualize real-time data exchange and is especially useful for demoing
+// how routes are triggered and how automatic Swagger documentation is generated.
+app.use(morgan("dev")); // Logs method, URL, status, and response time
+
+// Register only global/common middlewares here
+
+// No request-handling logic or route-specific middleware belongs in this file
 app.use(express.json());
 app.use(cookieParser());
 app.use(authMiddleware);
