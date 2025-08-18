8e55acc60ac6470503bc7d54ef4f02c4439c4908
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-08-18 14:23:42 +0200

    Chore: init (re-using alejandros initial changes)

---
+++ src/controllers/cloudinaryController/cloudinaryController.js
@@ -0,0 +1,47 @@
+import cloudinary from "../../config/cloudinary.js";
+import { logError } from "../../util/logging.js";
+
+export const cloudinaryController = async (req, res) => {
+  const file = req.file; // multer saves the file in req.file
+
+  if (!file) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "No file received by controller" });
+  }
+
+  try {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: "user_profiles",
+      },
+      (error, result) => {
+        if (error) {
+          return res.status(500).json({
+            success: false,
+            msg: "Image upload failed",
+            error: error.message,
+          });
+        }
+
+        res.status(200).json({
+          success: true,
+          msg: "File uploaded!",
+          url: result.secure_url,
+          result,
+        });
+      },
+    );
+
+    uploadStream.end(file.buffer);
+  } catch (error) {
+    logError(error);
+    return res.status(500).json({
+      success: false,
+      msg: "Unexpected server error during upload",
+      error: error.message,
+    });
+  }
+};
+
+export default cloudinaryController;
